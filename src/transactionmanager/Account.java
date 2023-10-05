a533359563d17d33c28299c6fe65d3fd10421b51
Mohammed Salama <mhs170@scarletmail.rutgers.edu>
2023-10-05 01:20:07 -0400

    find, grow, contains, open, and close methods in AccountDatabase.java

--- src/transactionmanager/Account.java
+++ src/transactionmanager/Account.java
@@ -3,6 +3,17 @@ package transactionmanager;
 public abstract class Account implements Comparable<Account> {
     protected Profile holder;
     protected double balance;
+
     public abstract double monthlyInterest();
+
     public abstract double monthlyFee();
+
+    public Account(Profile holder, double balance, double monthlyInterest,
+                   double monthlyFee) {
+        this.holder = holder;
+        this.balance = balance;
+        this.monthlyInterest();
+        this.monthlyFee();
+    }
+
 }
--- src/transactionmanager/AccountDatabase.java
+++ src/transactionmanager/AccountDatabase.java
@@ -1,14 +1,53 @@
 package transactionmanager;
 
 public class AccountDatabase {
+    private static final int NOT_FOUND = -1;
     private Account [] accounts; //list of various types of accounts
     private int numAcct; //number of accounts in the array
-    private int find(Account account) {} //search for an account in the array
-    private void grow() //increase the capacity by 4
-    public boolean contains(Account account){} //overload if necessary
-    public boolean open(Account account){} //add a new account
-    public boolean close(Account account){} //remove the given account
-    public boolean withdraw(Account account){} //false if insufficient fund
+    private int find(Account account) {
+        for (int i = 0; i < numAcct; i++) {
+            if (accounts[i].equals(account)) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
+    } //search for an account in the array
+    private void grow(){
+        Account[] newEventArray = new Account[accounts.length + 4];
+        for (int i = 0; i < accounts.length; i++) {
+            newEventArray[i] = accounts[i];
+        }
+        accounts = newEventArray;
+    } //increase the capacity by 4
+    public boolean contains(Account account){
+        if (find(account) != -1) {
+            return true;
+        }
+        return false;
+        //overload if necessary
+    }
+    public boolean open(Account account){
+        if (numAcct == accounts.length){
+            grow();
+        }
+        accounts[numAcct] = account;
+        numAcct += 1;
+        return true;
+    } //add a new account
+    public boolean close(Account account){
+        int accountIndex = find(account);
+        if (accountIndex != -1) {
+            for (int i = accountIndex; i < accounts.length - 1; i++) {
+                accounts[i] = accounts[i + 1]; //removes the event
+            }
+            accounts[accounts.length - 1] = null; //sets last element to null
+            numAcct -= 1;
+            return true;
+        }
+        return false;
+    } //remove the given account
+    public boolean withdraw(Account account){
+    } //false if insufficient fund
     public void deposit(Account account){}
     public void printSorted(){} //sort by account type and profile
     public void printFeesAndInterests(){} //calculate interests/fees
