ea65e26801ad7dd5fad3344264320f7800b79b1a
Dakshal P <dakshal29@gmail.com>
2023-10-06 17:18:25 -0400

    add TransactionManager base

--- src/transactionmanager/TransactionManager.java
+++ src/transactionmanager/TransactionManager.java
@@ -1,5 +1,32 @@
 package transactionmanager;
 
+import java.util.Scanner;
+
 public class TransactionManager {
-    public void run(){}
+    public void run(){
+        System.out.println("Transaction Manager is running.\n");
+
+        Scanner scanner = new Scanner(System.in);
+        scannerLoop: while (scanner.hasNextLine()) {
+            String commandLine  = scanner.nextLine();
+            String[] inputs     = commandLine.split("\\s+");
+            String command      = inputs[0];
+
+            switch (command) {
+                case "O" -> {}
+                case "C" -> {}
+                case "D" -> {}
+                case "W" -> {}
+                case "P" -> {}
+                case "PI" -> {}
+                case "UB" -> {}
+                case "Q"    -> {
+                    System.out.println("Transaction Manager is terminated.");
+                    break scannerLoop;
+                }
+                case "" -> {}
+                default -> System.out.println("Invalid command!");
+            }
+        }
+    }
 }
